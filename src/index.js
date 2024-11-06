e00a694523d07e4f84c2ea878a155da058da10f2
pig <panda14659@gmail.com>
2024-11-07 02:31:51 +0800

    test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import User from "./pages/user/User";
 import NotFound from "./pages/NotFound";
 
+// 使用 ReactDOM 的 createRoot 將 React 應用綁定到 HTML 中 id 為 root 的元素上
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
